62462c66e64cd48a2f6851a92f6cd96a2641f103
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2023-11-12 23:10:18 -0500

    Added Vehicle Routes

--- app/router/vehicleRouter.js
+++ app/router/vehicleRouter.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
+const { create, getAll, getById, updateById, deleteById, vehicleNotFound } = require("../controller/vehicleController");
+
+//Create
+router.post("/", create);
+
+//Read
+router.get("/", getAll);
+
+//Read By ID
+router.get("/id/:id", getById);
+
+//Update By ID
+router.put("/id/:id", updateById);
+
+//Delete By ID
+router.delete("/id/:id", deleteById);
+
+//Not found handler
+router.use(vehicleNotFound);
 
 module.exports = router;
