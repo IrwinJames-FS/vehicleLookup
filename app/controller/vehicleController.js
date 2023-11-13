afd689a47eb58bbd061c527b026bf89eea09ca6a
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2023-11-12 23:04:38 -0500

    VehicleController functions finished

--- app/controller/vehicleController.js
+++ app/controller/vehicleController.js
@@ -22,10 +22,55 @@ const getAll = async (_, res, next) => {
  * @param {*} next 
  */
 const getById = async (req, res, next) => {
+	const { id } = req.params;
 	try {
-		const vehicle = await Vehicle.findById().populate().exec();
-		return res.status(200).json(vehicle);
+		const vehicle = await Vehicle.findById(id).populate().exec();
+		return vehicle ? res.status(200).json(vehicle):next();
 	} catch (error) {
 		return next(error);
 	}
+}
+
+/**
+ * Update Vehicle by ID
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const updateById = async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const vehicle = await Vehicle.findByIdAndUpdate(id, req.body, {new: true});
+		return vehicle ? res.status(202).json(vehicle):next();
+	} catch (error) {
+		return next(error);
+	}
+}
+
+/**
+ * Delete Vehicle by ID
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const deleteById = async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const vehicle = await Vehicle.findByIdAndDelete(id);
+		return res.status(vehicle ? 202:204).json(vehicle);
+	} catch (error) {
+		return next(error);
+	}
+}
+
+/**
+ * Vehicle not found middleware
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next  
+ */
+const vehicleNotFound = (req, res, next) => {
+	const error = new Error("Vehicle not found!");
+	error.status = 404;
+	return next(error);
 }
